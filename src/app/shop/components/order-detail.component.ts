9510679df34e32c0acb7763a5f3373ff6371ad3f
truong04052003 <tranvantruong10b2@gmail.com>
2023-02-09 22:51:10 +0700

    checkout_form

--- src/app/shop/components/order-detail.component.ts
+++ src/app/shop/components/order-detail.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './../templates/order-detail.component.html',
 })
-export class OrderDetailComponent {
+export class OrderDetailComponent implements OnInit{
+  constructor( ) { }
 
+  ngOnInit(): void {
+  }
 }
